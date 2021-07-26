f4b2bd0799fc39f52c073f9ab2e5200e0c6cce88
redred <imbapalagz@gmail.com>
2021-07-26 19:38:10 +0800

    transferred course name and parts to single object

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,16 +8,16 @@ const Header = (props) => {
 
 const Part = (props) => {
 	return (
-		<p>{props.part} {props.exercise}</p>
+		<p>{props.part} {props.exercises}</p>
 	)
 }
 			
 const Content = (props) => {
 	return (
-		<div>
-			<Part part={props.part1} exercise={props.exercise1}/>
-			<Part part={props.part2} exercise={props.exercise2}/>
-			<Part part={props.part3} exercise={props.exercise3}/>
+		<div>		
+			{props.parts.map(function(part_obj, i) {
+				return <Part part={part_obj.name} exercises={part_obj.exercises} key={i}/>
+			})}
 		</div>
 	)
 }
@@ -32,27 +32,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-  	{
-  		name: 'Fundamentals of React',
-  		exercises: 10
-  	},
-  	{
-  		name: 'Using props to pass data',
-  		exercises: 7
-  	},
-  	{
-  		name: 'State of a component',
-    	exercises: 14
-  	}
-  ]
+  const course = {
+  	name: 'Half Stack application development',
+  	parts: [
+  		{
+  			name: 'Fundamentals of React',
+  			exercises: 10
+  		},
+  		{
+  			name: 'Using props to pass data',
+  			exercises: 7
+  		},
+  		{
+  			name: 'State of a component',
+    		exercises: 14
+  		}
+  	]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-	
-      <Total parts={parts}/>
+      <Header course={course.name}/>
+			<Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
