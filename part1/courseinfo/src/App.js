5708c0558614c5433ecf3c57c4fbf0f0d3c55679
redred <imbapalagz@gmail.com>
2021-07-26 19:31:15 +0800

    made parts an array transferred total computation to component todo: convert content to reflect aforementioned change to parts

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,38 +23,36 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+	const reducer = (accumulator, current_part) => accumulator + current_part.exercises
+	const sum = props.parts.reduce(reducer, 0);
+
 	return (
-		<p>Number of exercises {props.sum}</p>
+		<p>Number of exercises {sum}</p>
 	)
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-  	name: 'Fundamentals of React',
-  	exercises: 10
-  }
-  const part2 = {
-  	name: 'Using props to pass data',
-  	exercises: 7
-  }
-  const part3 = {
-  	name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+  	{
+  		name: 'Fundamentals of React',
+  		exercises: 10
+  	},
+  	{
+  		name: 'Using props to pass data',
+  		exercises: 7
+  	},
+  	{
+  		name: 'State of a component',
+    	exercises: 14
+  	}
+  ]
 
   return (
     <div>
       <Header course={course}/>
-			<Content
-				part1={part1.name}
-				exercise1={part1.exercises}
-				part2={part2.name}
-				exercise2={part2.exercises}
-				part3={part3.name}
-				exercise3={part3.exercises}
-			/>
-      <Total sum={part1.exercises + part2.exercises + part3.exercises}/>
+	
+      <Total parts={parts}/>
     </div>
   )
 }
